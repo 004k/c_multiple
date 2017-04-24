8a9189523fc13382b82ce1b467b5e3d8babc5cf3
Kaito Ishii <ht16a004@oecu.jp>
2017-04-24 09:11:05 +0900

    Initial commit

---
+++ common_multiple.cpp
@@ -0,0 +1,40 @@
+// common_multiple.cpp
+#include <iostream>
+using namespace std;
+
+int dif(const int num);
+int input(int a,int b);
+int calculate(int a,int b);
+
+int main()
+{
+	int num;
+
+	dif(num);
+
+	int*a =new int[num];
+
+	input(a[num]);
+	calculate();
+
+	delete[] a;
+
+	cin.get();
+	return 0;
+}
+
+int dif(int num)
+{
+	cout<<"Œö”{”‚ğ‹‚ß‚é—v‘f”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F ";
+	cin>>num;
+	return num;
+}
+
+int input(int a[num])
+{
+	cout<<"1‚Â‚ß‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F ";
+	cin>>a;
+	cout<<"2‚Â‚ß‚Ì®”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢F ";
+	cin>>b;
+	return a,b;
+}
